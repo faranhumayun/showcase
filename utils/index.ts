3a494a9be5109e500df6b85f357acf993bcf045d
Faran Humayun <faranhumayun@gmail.com>
2024-12-29 23:53:14 +0500

    adding integrations

--- utils/index.ts
+++ utils/index.ts
@@ -14,24 +14,3 @@ const urlLink = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla
 //     console.log(error)
 // }
 
-// async function apiFetch() {
-//     try {
-//     const response = await fetch(urlLink)
-//     const data = await response.json()
-//     console.log("Data:---", data)
-// } catch (error) {
-//     console.log(error)
-// }
-// }
-// apiFetch()
-
-fetch(urlLink)
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    console.log("DATA -- ", data)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
