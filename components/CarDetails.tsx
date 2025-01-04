65e234d6a7e850d2e3721eed4bd3041a688b9cbb
Faran Humayun <faranhumayun@gmail.com>
2025-01-04 16:22:44 +0500

    car detail component edit

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,6 +1,6 @@
 import { CarProps } from '@/types'
 import React, { Fragment } from 'react'
-import { Dialog, Transition, TransitionChild } from '@headlessui/react'
+import { Description, Dialog, DialogBackdrop, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react'
 import Image from 'next/image'
 
 interface CarDetailsProps {
@@ -12,25 +12,26 @@ interface CarDetailsProps {
 const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
   return (
     <>
-    <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as='div' className='relative z-10' onClose={closeModal}>
-        <TransitionChild
-          as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
-        >
-          <div className='fixed inset-0 bg-black bg-opacity-25' />
-        </TransitionChild>
-
-        {/* <div className='fixed inset-0 overflow-y-auto'> */}
-          
-        {/* </div> */}
+    <Dialog open={isOpen} onClose={closeModal} className="relative z-50">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-black/30 duration-300 ease-out data-[closed]:opacity-0"
+        />
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+          <DialogPanel
+            transition
+            className="max-w-lg space-y-4 bg-white p-12 duration-300 ease-out data-[closed]:scale-95 data-[closed]:opacity-0"
+          >
+            <DialogTitle className="text-lg font-bold">Deactivate account</DialogTitle>
+            <Description>This will permanently deactivate your account</Description>
+            <p>Are you sure you want to deactivate your account? All of your data will be permanently removed.</p>
+            <div className="flex gap-4">
+              <button onClick={closeModal}>Cancel</button>
+              {/* <button onClick={() => setIsOpen(false)}>Deactivate</button> */}
+            </div>
+          </DialogPanel>
+        </div>
       </Dialog>
-    </Transition>
   </>
   )
 }
